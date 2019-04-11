c3df1a39abed668979d45a712f05cf253b435032
Ana <anamarirc23@gmail.com>
2019-04-10 21:32:31 -0500

    primer commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/*// Module to control application life. (this variable should already exist)
+const app = electron.app
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent(app)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}*/
+
 if (environment.production) {
   enableProdMode();
 }
